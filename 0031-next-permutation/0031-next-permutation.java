5310639dbee2f32bd1c86d90a5fab16084d1c116
Nikhil Patel <nikhilpatel64581@gmail.com>
2025-07-19 08:24:31 +0530

    Time: 0 ms (100%), Space: 43 MB (74.32%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,47 +1,31 @@
-import java.util.Arrays;
-
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int pivot = -1;
+        int i = nums.length - 2; // Start from second last element
 
-        // Step 1: Find pivot Right से ऐसा पहला element ढूंढना जो अपने अगले से छोटा हो
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                pivot = i;
-                break;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
             }
+            swap(nums, i, j);
         }
 
-        // Step 2: If no pivot, reverse entire array  अगर ऐसा कोई element नहीं मिला, तो पूरी array को उल्टा कर दो
-        if (pivot == -1) {
-            reverse(nums, 0, n - 1);
-            return;
-        }
-
-        // Step 3: Find next greater element on right of pivot Pivot के दाएँ सबसे छोटा बड़ा element ढूंढना
-        for (int i = n - 1; i > pivot; i--) {
-            if (nums[i] > nums[pivot]) {
-                swap(nums, i, pivot);
-                break;
-            }
-        }
-
-        // Step 4: Reverse the suffix Pivot के बाद वाले हिस्से को उल्टा कर दो (ascending में लाना)
-        reverse(nums, pivot + 1, n - 1);
-    }
- 
-    // Utility to reverse array between two indices एक हिस्से को उल्टा करना
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start++, end--);
-        }
+        reverse(nums, i + 1, nums.length - 1);
     }
 
-    // Utility to swap two elements दो index के elements को बदलना
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    private void reverse(int[] nums, int st, int ed) {
+        while (st < ed) {
+            swap(nums, st++, ed--);
+        }
     }
 }
