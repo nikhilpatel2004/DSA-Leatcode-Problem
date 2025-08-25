0b2427370d8935220c5ff53bb9f178f285ac89c9
Nikhil Patel <nikhilpatel64581@gmail.com>
2025-08-25 21:18:54 +0530

    Time: 12 ms (58.53%), Space: 43.1 MB (16.27%) - LeetHub

---
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static int countVowelSubstrings(String word) {
+        
+    	int n = word.length();
+    	int count = 0;
+    	
+    	for (int i = 0; i < n; i++) {
+			
+    		Set<Character> hs = new HashSet<Character>();
+    		
+    		for (int j = i; j < n; j++) {
+				
+    			char ch =  word.charAt(j);
+    			
+    			if(isVowel(ch)) {
+    				hs.add(ch);
+    				
+    				if(hs.size()==5) {
+    					count++;
+    				}
+    				
+    			}else {
+    				break;
+    			}
+    		
+    			
+			}
+    		
+		}
+    	
+    	return count;
+
+    	
+    }
+	
+	private static boolean isVowel(char ch) {
+		return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+	}
+}
